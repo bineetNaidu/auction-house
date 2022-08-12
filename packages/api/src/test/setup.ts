819f102b43eed78aa4d306241968919bf9fcaa8b
bineetNaidu <bineet.s.naidu@gmail.com>
2022-08-12 21:48:55 +0530

    🚀 initialized api

---
+++ packages/api/src/test/setup.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../data-source';
+
+let conn: DataSource;
+
+beforeAll(async () => {
+  conn = await AppDataSource.initialize();
+});
+
+afterAll(async () => {
+  await conn.dropDatabase();
+  await conn.destroy();
+});
